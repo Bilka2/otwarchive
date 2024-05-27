a8cf25775f29e8a398d6a83670ce5009a91336c4
Bilka <Bilka2@users.noreply.github.com>
2024-05-27 22:13:40 +0200

    Load ICE js in mailer previews

--- config/initializers/monkeypatches/mailers_controller.rb
+++ config/initializers/monkeypatches/mailers_controller.rb
@@ -6,6 +6,13 @@ module MailersController
     skip_rack_dev_mark
   end
 end
+module ViewMailersController
+  def self.prepended(base)
+    # Use modified email view so it can include the phrase in context editor
+    base.prepend_view_path Rails.root.join('test', 'mailers', 'previews')
+  end
+end
 Rails.application.config.after_initialize do
+  ::Rails::MailersController.prepend ViewMailersController
   ::Rails::MailersController.include MailersController
 end
