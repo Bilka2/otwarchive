7ae3718670cae809afb7bbb0febf70d7f03ca59b
Bilka <Bilka2@users.noreply.github.com>
2024-05-30 21:56:31 +0200

    Revert user mailer preview change

--- config/initializers/phraseapp_in_context_editor.rb
+++ config/initializers/phraseapp_in_context_editor.rb
@@ -10,7 +10,7 @@ PhraseApp::InContextEditor.configure do |config|
   # project id in your project settings and account id in your account
   # settings (https://app.phrase.com/)
   config.project_id = "a3667b8095533c2b3f8d3ac946bb642f"
-  config.account_id = "foo"
+  config.account_id = "c14670ca"
 
   # Configure an array of key names that should not be handled
   # by the In-Context-Editor.
